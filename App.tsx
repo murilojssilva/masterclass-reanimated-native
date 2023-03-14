37a686ae42e05076b7510452ee7f49e66455720e
Murilo de Jesus Santos Silva <murilojssilva@outlook.com>
2023-03-14 20:46:34 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Pressable, Text, Button } from "react-native";
+import { styles } from "./styles";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+  Easing,
+  interpolateColor,
+  withSequence,
+} from "react-native-reanimated";
+
+/**
+ * useAnimatedStyles
+ * useSharedValue
+ * Animated.View
+ * withSpring: efeito de mola
+ * withTiming: transição suave
+ * Easing: elastic(n), bounce
+ * interpolateColor: transição de cores
+ */
+
+/**
+ * Criar elementos animados caso não esteja disponível
+ * const PressableAnimated = Animated.createAnimatedComponent(Pressable);
+ */
 
 export default function App() {
+  const scale = useSharedValue(1);
+  const translateX = useSharedValue(0);
+
+  function handleScaleAnimation(value: number) {
+    scale.value = withTiming(value, {
+      duration: 700,
+      easing: Easing.bounce,
+    });
+  }
+
+  function handleTranslateXAnimation() {
+    scale.value = withTiming(1.5, { duration: 3000 }, (finished) => {
+      if (finished) {
+        translateX.value = withSequence(
+          withTiming(150),
+          withTiming(-150),
+          withSpring(0, undefined, (finished) => {
+            if (finished) {
+              scale.value = withSpring(1);
+            }
+          })
+        );
+      }
+    });
+  }
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }, { translateX: translateX.value }],
+    backgroundColor: interpolateColor(scale.value, [1, 1.5], ["blue", "red"]),
+  }));
+
+  const pressableAnimatedStyle = useAnimatedStyle(() => ({
+    backgroundColor: interpolateColor(scale.value, [1, 1.5], ["blue", "red"]),
+  }));
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View style={[styles.box, animatedStyle]} />
+      <Pressable
+        onPressIn={() => handleScaleAnimation(1.5)}
+        onPressOut={() => handleScaleAnimation(1)}
+      >
+        <Text style={styles.button}>Clique aqui</Text>
+      </Pressable>
+      <Button title="Animar" onPress={handleTranslateXAnimation} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
